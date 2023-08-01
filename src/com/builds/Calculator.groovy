20b7eaed81fbc3862695d9a5292961c08466ace1
branchrule <branchrule@gmailc.om>
2023-08-01 11:42:41 +0530

    added

--- src/com/builds/Calculator.groovy
+++ src/com/builds/Calculator.groovy
@@ -1,10 +1,3 @@
-public class Calculator{
-
-def jenkins;
-
-Calculator(jenkins){
-    this.jenkins=jenkins;
-}
 
 def add(firstNumber,secondNumber){
     echo "Added Numbers ${firstNumber+secondNumber}"
@@ -15,4 +8,3 @@ def mul(firstNumber,secondNumber){
    echo "Mul  Numbers ${firstNumber*secondNumber}"
 }
 
-}
